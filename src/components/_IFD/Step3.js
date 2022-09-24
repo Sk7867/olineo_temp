966cbfc6a742cea152ddc7c9cf0cf67a0f3046e7
Devansh Soni <devanshsoni811@gmail.com>
2022-09-25 01:15:42 +0530

    IFD UX improvement and bug fix

--- src/components/_IFD/Step3.js
+++ src/components/_IFD/Step3.js
@@ -67,6 +67,7 @@ const Step3 = ({ setCounterWidth, stepCounterSvgRef, navigateBackward, navigateF
     const phonesData = await response.json();
     setProductDetails((prev) => ({ ...prev, phonesData }));
   };
+
   useEffect(() => {
     if (productDetails.selectedCategory === "phones") {
       getPhonesData();
